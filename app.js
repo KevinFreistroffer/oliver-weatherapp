1ab74a953938de5a037cdc617e4cfc4ddd556e01
Kevin <kevin.freistroffer@gmail.com>
2019-07-11 16:09:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '')));
+
+
+app.use('/*', (req, res) => {
+	res.sendFile(express.static(path.join(__dirname, './index.html')));
+});
+
+app.listen((process.env.PORT || 1337), () => {
+	console.log(`Listening on ${(process.env.PORT || 1337)}`)
+});
+
+module.exports = app;
---
+++ precache-manifest.5ccfcd98f72523e13094d668f11ec522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86025c8675220c9a997437735e47f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "779f7441821f132d2c1e",
+    "url": "/static/css/main.16298f39.chunk.css"
+  },
+  {
+    "revision": "3bd2bcc0ecfc3fabf548",
+    "url": "/static/js/2.9d28795b.chunk.js"
+  },
+  {
+    "revision": "779f7441821f132d2c1e",
+    "url": "/static/js/main.b8212d15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
